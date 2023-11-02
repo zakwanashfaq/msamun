c07f562613f7325866c19e8cfaf0ee334b37f3b7
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2023-11-02 10:25:38 -0230

    updated contacts

--- client/src/pages/contacts/index.tsx
+++ client/src/pages/contacts/index.tsx
@@ -4,9 +4,29 @@ import PageTemplate from "@/components/pageTemplate";
 export default function ExecutivesPage() {
   return (
     <PageTemplate>
-      <div className="d-flex justify-content-center align-items-center p-5 text-wrap">
-        <h1 className="fw-bold">This page is under construction. Sorry for the inconvenience...</h1>
+      <div className="d-flex flex-column justify-content-center align-items-start">
+        <div className="pb-5">
+          <div className="card-body">
+            <h5 className="card-title fw-bold pb-3">Office Location</h5>
+            <p className="mb-0">University Center (UC) 6004</p>
+            <p className="mb-0">Memorial University</p>
+          </div>
+        </div>
+        <div className="pb-5">
+          <div className="card-body">
+            <h5 className="card-title fw-bold pb-3">Contact Information</h5>
+            <p className="mb-0">Facebook Group: <a className="fw-bold" href="https://www.facebook.com/groups/7510890852" target="_blank">https://www.facebook.com/groups/7510890852</a></p>
+            <p className="mb-0">Email: <a className="fw-bold">msamun@mun.ca</a></p>
+          </div>
+        </div>
+        <div className="pb-5">
+          <div className="card-body">
+            <h5 className="card-title fw-bold pb-3">Community updates</h5>
+            <p className="mb-0">We have a facebook-messenger group for quick updates, like changes to jumma prayer, Eid events etc. </p>
+            <p className="mb-0">Please ask an executive, email or reach us out through our facebook group page to be added to the chat.</p>
+          </div>
+        </div>
       </div>
-    </PageTemplate>
+    </PageTemplate >
   )
 }
